a6623afd734dc29b7d9f68f452a2a07d3dff7797
MHD_NF <MHD_NF@github.com>
2017-11-10 19:12:22 +0200

    Add select object (RayCast2D)

---
+++ ContaCityProject1/Assets/Scripts/GlobaScripts/Checker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Checker : MonoBehaviour {
+
+	public static bool ObjectDoesNotHave(GameObject theObject, Type comp){
+
+		var com = theObject.GetComponent( comp );
+
+		if (com == null) {
+			Debug.LogError ("The Object Named: " + theObject.name + " don't have: " + comp.ToString());
+			return true;
+		}
+
+		//print ("The Object Named: " + theObject.name + " has : " + comp.ToString() + " on it");;
+		return false;
+
+	}
+
+
+	public static bool IsCloseEnough(Vector3 posOne, Vector3 posTwo, float range){
+	
+		if( Vector3.Distance(posOne,posTwo) < range) {
+			return true;
+		}else{
+			return false;
+		}
+	
+	}
+
+
+	public static bool IsLayerInLayerMask(LayerMask mask, int layer){
+		return mask == (mask | (1 << layer));
+	}
+
+
+}
---
+++ ContaCityProject1/Assets/Scripts/GlobaScripts/GlobalVars.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+namespace GlobalVars {
+
+
+	#region public Enums
+
+//**	public enum PrefabTypes { None, Player ,Bullet, Punchy , Rocket, UIMessage, Coin, SwitchStateEffect};
+
+	//** public enum SFXtype { None, Coin };
+
+
+	//** public enum MonstersType { None, Punch };
+
+	//** public enum StateType { None, Healing, Moving, Running, Attacking };
+
+	#endregion
+
+
+	#region Public Delegates
+
+//**	public delegate void Action ();
+
+	//**	public delegate void MB (MonsterBrain mb);
+
+	#endregion	
+
+
+	public class Events : MonoBehaviour {
+
+
+		#region Color Scene
+		public delegate void OneColorEvent();
+		public static event OneColorEvent ChangeToRed;
+
+		public static void CallChangeToRed(){
+			ChangeToRed ();
+		}
+
+		public delegate void TowColorEvent(Color col);
+		public static event TowColorEvent ChangeToCol;
+
+		public static void CallChangeToCol(Color col){
+			ChangeToCol (col);
+		}
+
+		#endregion
+
+
+
+		#region Rocket Scene
+
+		#endregion
+
+
+
+		#region TopDownShooter Scene
+
+
+//		public delegate void Event();
+//		public static event Event OnPlayerDeath, OnSceneLeave;
+
+	//	public static void ActivateOnPlayerDeath(){
+	//		if (OnPlayerDeath != null)
+	//			OnPlayerDeath ();
+	//		else
+	//			Debug.LogError ("OnPlayerDeath is empty!");
+	//	}
+	//	public static void CallOnSceneLeave(){
+	//		OnSceneLeave ();
+	//	}
+
+
+
+	//	public delegate void GameObjectEvent(GameObject player);
+	//	public static event GameObjectEvent OnPlayerSpawn;
+
+	//	public static void ActivateOnPlayerSpawn(GameObject player){
+	//		OnPlayerSpawn (player);
+	//	}
+	//
+
+		#endregion
+
+
+	}
+
+
+}
+
+

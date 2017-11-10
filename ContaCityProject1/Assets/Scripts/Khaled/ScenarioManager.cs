818f8be6bc5472c7e2c1a7c1900486f21a22aeb7
khaled <khaled.dallah0@gmail.com>
2017-11-10 13:38:46 -0800

    Add test things 2

--- ContaCityProject1/Assets/Scripts/Khaled/ScenarioManager.cs
+++ ContaCityProject1/Assets/Scripts/Khaled/ScenarioManager.cs
@@ -3,31 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ScenarioManager : MonoBehaviour {
-
+	
 	public GameObject CarFollow;
 	public GameObject Fire1;
 	public GameObject Fire2;
+
 	private FollowPathUnit followPathUnit;
+	private float delayFire1=4f;
+	private float delayFire2=6f;
+	private float startTime;
+
+
 	// Use this for initialization
 	void Start () {
+		startTime = Time.time;
+		Fire1.SetActive (false);
+		Fire2.SetActive (false);
 		followPathUnit = CarFollow.GetComponent<FollowPathUnit> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if ( Input.GetKey(KeyCode.A) ) {
+		if (Input.GetKey (KeyCode.A)) {
 			followPathUnit.followPathTrig = true;
 			print ("a");
 		}
-		Example ();
+
+		if (Time.time >= startTime + delayFire1) {
+			Fire1.SetActive (true);
+		}
+
+		if (Time.time >= startTime + delayFire2) {
+			Fire2.SetActive (true);
+		}
 	}
 
-	IEnumerator Example() {
-		print(Time.time);
-		yield return new WaitForSeconds(5);
-		Fire1.SetActive (true);
-		yield return new WaitForSeconds(5);
-		Fire2.SetActive (true);
-		print(Time.time);
-	}
+
 }
